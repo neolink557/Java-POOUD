118c28f28e27340624adf5289d666f38828af8d9
neolink557 <neolink557@gmail.com>
2018-08-13 18:57:21 -0500

    agregue el ciclos-34

---
+++ JavaApplication1/src/javaapplication1/Ciclos34.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+import java.util.Scanner;
+public class Ciclos34 
+{
+    public static void main(String[] args) {
+        int i=1;
+        do
+        {
+            System.out.println(i++);
+        }while(i<=10);
+    }
+}
